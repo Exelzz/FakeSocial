af2b55b1c1335b7c1653df8009c8246445403400
Exelzz <dogukan32145613@hotmail.com>
2022-06-20 16:49:34 +0300

    fakesocial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 if (module.hot) {
   module.hot.accept();
@@ -9,48 +10,9 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-    </div>
-      <div className="comment">
-         <a href="/" className="avatar">
-           <img alt="avatar" src={faker.image.image()} />
-         </a>
-         <div className="content">
-           <a href="/" className="author">
-             Sam
-           </a>
-           <div className="metadata">
-             <span className="date">Today at 6:00PM</span>
-           </div>
-           <div className="text">Nice blog post!</div>
-         </div>
-      </div>
-      <div className="comment">
-         <a href="/" className="avatar">
-           <img alt="avatar" src={faker.image.image()} />
-         </a>
-         <div className="content">
-           <a href="/" className="author">
-             Sam
-           </a>
-           <div className="metadata">
-             <span className="date">Today at 6:00PM</span>
-           </div>
-           <div className="text">Nice blog post!</div>
-         </div>
-      </div>
+      <CommentDetail author="Eduard" timeAgo="Today at 4:45PM" content="bu gün guzel bir gun" />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="Evet gerçekten güzel bir gün" />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="benim için aynısın söyliyemem" />
     </div>
   );
 };
